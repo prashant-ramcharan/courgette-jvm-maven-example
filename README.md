71dc566e35fde623d8d4bc4736ad8fe5e1a816e9
Prashant Ramcharan <info@prashant-ramcharan.co.uk>
2018-09-11 18:34:18 +0100

    Courgette JVM and Maven example.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# courgette-jvm-maven-example
+# Courgette-JVM Example
+
+An example project showing how to use Courgette-JVM with Maven.
+
+## System Requirements
+
+* Google Chrome
+* MacOS / Windows / Linux
+
+## Test Execution
+        
+* **Parallel Scenario Execution using JUnit**
+    * Option 1: Run _suites/ScenarioSuite.java_ like a normal JUnit test.
+    
+    * Option 2: Execute Maven goal
+    ````maven
+        mvn test
+    ````
---
+++ src/test/java/steps/TestSteps.java
@@ -0,0 +1,61 @@
+package steps;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
+import io.github.pramcharan.wd.binary.downloader.WebDriverBinaryDownloader;
+import io.github.pramcharan.wd.binary.downloader.enums.BrowserType;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSteps {
+    private WebDriver driver;
+
+    @Before
+    public void before() {
+        WebDriverBinaryDownloader.create().downloadLatestBinaryAndConfigure(BrowserType.CHROME);
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void after(Scenario scenario) {
+        if (scenario.isFailed()) {
+            scenario.write("Scenario failed so capturing a screenshot");
+
+            TakesScreenshot screenshot = (TakesScreenshot) driver;
+            scenario.embed(screenshot.getScreenshotAs(OutputType.BYTES), "image/png");
+        }
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Given("^I navigate to Stack Overflow$")
+    public void iNavigateToStackOverflow() {
+        driver.navigate().to("https://stackoverflow.com/");
+    }
+
+    @When("I navigate to Stack Overflow question page (\\d+)")
+    public void navigateToStackOverflowQuestionPage(Integer page) {
+        driver.navigate().to("https://stackoverflow.com/questions?page=" + page);
+    }
+
+    @Then("I verify Stack Overflow question page (\\d+) is opened")
+    public void verifyCorrectQuestionPageIsOpened(Integer page) {
+        if (!driver.getTitle().contains("Page " + page)) {
+            throw new RuntimeException("The Stack Overflow page title does not contain the page number: " + page);
+        }
+    }
+
+    @When("I use the following data table to navigate to a Stack Overflow question page")
+    public void useTheFollowingDataTable(DataTable dataTable) {
+        int pageNumber = Integer.valueOf(dataTable.cell(1, 0));
+        navigateToStackOverflowQuestionPage(pageNumber);
+    }
+}
